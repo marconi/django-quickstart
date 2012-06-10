11a1c3343114307dbebee35c7724b4e75c365624
Marconi Moreto <caketoad@gmail.com>
2012-06-10 22:50:07 +0800

    adding piggy backing in fields

--- todo/forms.py
+++ todo/forms.py
@@ -12,13 +12,11 @@ class TodoListForm(forms.Form):
     def __init__(self, *args, **kwargs):
         todos = kwargs.pop('todos', [])
         super(TodoListForm, self).__init__(*args, **kwargs)
-        self.done_items = []
         for todo in todos:
             field = str(todo.id)
-            if todo.is_done:
-                self.done_items.append(field)
             self.fields[field] = forms.BooleanField(
                 required=False, label=todo.name)
+            self.fields[field].is_done = todo.is_done
 
     def clean(self):
         selected = [tid for tid, val in self.cleaned_data.items() if val]
